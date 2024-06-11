471b93b168da6eb6cb6fea783d8b320e214f249f
Ker Tenepere <Ker10epere@gmail.com>
2024-06-12 00:06:55 +0800

    fix error

--- nodeclient-fileuploader/src/index.ts
+++ nodeclient-fileuploader/src/index.ts
@@ -1,18 +1,33 @@
 import axios from 'axios'
 import { fileFromPath } from 'formdata-node/file-from-path'
 import path from 'path'
+
 const filesDir = path.join(__dirname, '../', 'files')
+
+const ResourceServerClient = axios.create({
+  baseURL: 'http://localhost:8080',
+})
 
 ;(async () => {
   const form = new FormData()
   form.append('file', await fileFromPath(path.join(filesDir, 'file.txt')))
   form.append('content', await fileFromPath(path.join(filesDir, 'content.txt')))
   form.append('blob', new Blob(['Some file']))
+  form.append('ignore', 'ignore')
 
   try {
-    const resp = await axios.post('http://localhost:8080/files', form)
+    const resp = await ResourceServerClient.postForm('/files', form, {
+      headers: {
+        Accept: 'text/plain',
+        'Content-Type': 'multipart/form-data',
+      },
+    })
     console.log(resp.data)
   } catch (e) {
-    console.log(e)
+    if (e instanceof Error) {
+      console.log(e.stack)
+    } else {
+      console.log(e)
+    }
   }
 })()
--- resource-server/src/main/kotlin/com/pushforward/resourceserver/FileController.kt
+++ resource-server/src/main/kotlin/com/pushforward/resourceserver/FileController.kt
@@ -3,12 +3,14 @@ package com.pushforward.resourceserver
 import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/")
 class FileController {
-    @PostMapping(path = ["/files"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.TEXT_PLAIN_VALUE])
+    @PostMapping("files", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.TEXT_PLAIN_VALUE])
     fun post(@RequestPart file: Resource, @RequestPart content: Resource, @RequestPart blob: Resource): String {
         println(file.contentAsByteArray.toString(Charsets.UTF_8))
         println(content.contentAsByteArray.toString(Charsets.UTF_8))
